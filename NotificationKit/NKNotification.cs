03d9208b24bde03f92731dacb6b65cf831c1777b
P. Schunker <philipp.schunker@allianz.at>
2019-02-15 09:20:42 +0100

    initial repo commit

---
+++ NotificationKit/NKNotification.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NotificationKit {
+    public class NKNotification {
+        public string Text { get; set; }
+        public int Level { get; set; }
+    }
+}
